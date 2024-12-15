9d9cd4fae07e8a99831f75159582b7ccc1f90252
fizzycurry <nawsheen_m@outlook.com>
2024-12-15 23:00:30 +0600

    added code for serving files

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from functools import lru_cache
+from http.client import HTTPException
 from fastapi import FastAPI, UploadFile # type: ignore
+from fastapi.responses import FileResponse # type: ignore
 from dotenv import load_dotenv # type: ignore
 from getENV import getEnvironmentVariable
 from os import makedirs, path
+import mimetypes
 import uvicorn # type: ignore
 
 load_dotenv()
@@ -15,10 +17,7 @@ FILEPATH = getEnvironmentVariable("FILEPATH", str, "uploads/")
 # make sure path exists
 if not path.exists(FILEPATH):
     makedirs(FILEPATH)
-    print(f"Directory '{FILEPATH}' created.")
-else:
-    print(f"Directory '{FILEPATH}' already exists.")
-    
+
 
 @app.post("/port")
 async def port():
@@ -27,9 +26,7 @@ async def port():
 @app.post("/upload/")
 async def uploadFile(file: UploadFile):
     try:
-        print(FILEPATH)
         filepath = path.join(FILEPATH, file.filename) 
-        print(filepath)
         with open(filepath, "wb") as f:
             while chunk := file.file.read(1024*1024):
                 f.write(chunk)
@@ -40,6 +37,18 @@ async def uploadFile(file: UploadFile):
         return {"message": "File could not be  uploaded"}
 
 
+@app.get("/view/", response_class=FileResponse)
+async def serveFile(filename: str):
+    filepath = path.join(FILEPATH, path.basename(filename))
+
+    if not path.exists(filepath) or not path.isfile(filepath):
+        raise HTTPException(status_code=404, detail="File not found")
+    
+    mime_type, _ = mimetypes.guess_type(filepath)
+    if mime_type is None:
+        raise HTTPException(status_code=400, detail="Unsupported or unknown file type")
+    
+    return FileResponse(filepath, media_type=mime_type)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=PORT, reload=True) #getting values from environment variables
